cbdd169c018c6f80e4802b18258695cd040c0460
quang luan <hoangquangluan23102001@gmail.com>
2023-11-25 13:29:58 +0700

    api_core

---
+++ Models/Student.cs
@@ -0,0 +1,16 @@
+namespace Core_WebAPI.Models
+{
+  public class Student
+  {
+    public string Msv { get; set; }
+    public string Lop { get; set; }
+    public string Khoavien { get; set; }
+    public string Cccd { get; set; }
+    public string Hodem { get; set; }
+    public string Ten { get; set; }
+    public string Bietdanh { get; set; }
+    public string Email { get; set; }
+    public string Dienthoai { get; set; }
+    public int Tuoi { get; set; }
+  }
+}
